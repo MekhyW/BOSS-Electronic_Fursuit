eb4d4cdfa9418b706504b1b41499009cfd77fd8a
Mekhy W.! <55763383+MekhyW@users.noreply.github.com>
2022-07-23 08:41:43 -0300

    rosrun rosserial_python

--- Mekhy_Engine/BOSSMEKHY.py
+++ Mekhy_Engine/BOSSMEKHY.py
@@ -8,6 +8,8 @@ import rospy
 from std_msgs.msg import UInt16, float32
 
 os.system("lxterminal -e roscore")
+os.system("lxterminal -e rosrun rosserial_python serial_node.py _port:=/dev/ttyACM0 _baud:=115200")
+os.system("lxterminal -e rosrun rosserial_python serial_node.py _port:=/dev/ttyACM1 _baud:=115200")
 rospy.init_node('BOSSMEKHY')
 expression_pub = rospy.Publisher('/expression', UInt16, queue_size=10)
 voice_pub = rospy.Publisher('/voice_volume', float32, queue_size=10)
