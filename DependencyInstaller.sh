93eb41ec5fe661279648c912860df7fb07cd0cd1
Felipe Catapano Emrich Melo <mefkhytheroboticist@gmail.com>
2022-12-19 09:11:45 -0300

    Create DependencyInstaller.sh

---
+++ DependencyInstaller.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+#
+# This script will install all the dependencies for the project.
+#
+# Usage:
+# ./DependencyInstaller.sh
+#
+# -----------------------------------------------------------------------------
+sudo apt-get update
+sudo apt-get install -y git-all
+sudo apt-get install -y python3-pip
+sudo apt install -y ffmpeg
+sudo apt install -y python3-gi
+pip3 install --upgrade pip
+pip3 install telepota
+pip3 install googletrans==3.1.0a0
+pip3 install opencv-python
+pip3 install numpy
+pip3 install pygame
+pip3 install pytube
+pip3 install gTTS
+pip3 install aubio
+pip3 install PyAudio
+pip3 install mediapipe-rpi4
+pip3 install tensorflow
+# JACK
+sudo apt-get install -y qjackctl
+sudo apt-get install -y jack-rack
+pip3 install JACK-Client
+# ROS
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu buster main" > /etc/apt/sources.list.d/ros-noetic.list'
+sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+sudo apt update
+sudo apt-get install -y python-rosdep python-rosinstall-generator python-wstool python-rosinstall build-essential cmake
+sudo rosdep init
+rosdep update
+mkdir ~/ros_catkin_ws
+cd ~/ros_catkin_ws
+rosinstall_generator ros_comm --rosdistro noetic --deps --wet-only --tar > noetic-ros_comm-wet.rosinstall
+wstool init src noetic-ros_comm-wet.rosinstall
+rosdep install -y --from-paths src --ignore-src --rosdistro noetic -r --os=debian:buster
+sudo dphys-swapfile swapoff
+sudoedit /etc/dphys-swapfile # Change CONF_SWAPSIZE=100 to CONF_SWAPSIZE=1024
+sudo dphys-swapfile setup
+sudo dphys-swapfile swapon
+sudo src/catkin/bin/catkin_make_isolated --install -DCMAKE_BUILD_TYPE=Release --install-space /opt/ros/noetic -j1 -DPYTHON_EXECUTABLE=/usr/bin/python3
+source /opt/ros/noetic/setup.bash
