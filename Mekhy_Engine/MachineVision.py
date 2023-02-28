4bb0fa39f76f916ecdaf686609c1fcafb8a29af5
MekhyW <mefkhytheroboticist@gmail.com>
2023-02-27 22:26:53 -0300

    constraint to displacement_eye, camera auto params, removed keras verbose

--- Mekhy_Engine/MachineVision.py
+++ Mekhy_Engine/MachineVision.py
@@ -13,6 +13,9 @@ left_eye_closed = False
 right_eye_closed = False
 AutomaticExpression = 'Neutral'
 cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, 3)
+cap.set(cv2.CAP_PROP_AUTO_WB, 1)
+cap.set(cv2.CAP_PROP_AUTOFOCUS, 1)
 mp_face_detection = mp.solutions.face_detection
 mp_face_mesh = mp.solutions.face_mesh
 mp_drawing = mp.solutions.drawing_utils
@@ -20,8 +23,8 @@ drawSpec = mp_drawing.DrawingSpec(thickness=1, circle_radius=2)
 face_detection = mp_face_detection.FaceDetection(min_detection_confidence=0.5)
 face_mesh = mp_face_mesh.FaceMesh(max_num_faces=1, refine_landmarks=True, min_detection_confidence=0.5, min_tracking_confidence=0.5)
 
-RIGHT_EYE =[ 362, 382, 381, 380, 374, 373, 390, 249, 263, 466, 388, 387, 386, 385,384, 398 ]
-LEFT_EYE=[ 33, 7, 163, 144, 145, 153, 154, 155, 133, 173, 157, 158, 159, 160, 161 , 246 ] 
+RIGHT_EYE =[362, 382, 381, 380, 374, 373, 390, 249, 263, 466, 388, 387, 386, 385,384, 398]
+LEFT_EYE=[33, 7, 163, 144, 145, 153, 154, 155, 133, 173, 157, 158, 159, 160, 161 , 246] 
 LEFT_IRIS = [468, 469, 470, 471, 472]
 RIGHT_IRIS = [473, 474, 475, 476, 477]
 LEFT_EYEBROW = [383, 300, 293, 334, 296, 336, 285, 417]
@@ -110,8 +113,8 @@ def inference_emotionrecog(image):
             faces.append(face)
             pos.append((x1, y1, x2, y2))
         x = recognition_preprocessing(faces)
-        y_1 = model_1.predict(x)
-        y_2 = model_2.predict(x)
+        y_1 = model_1.predict(x, verbose=0)
+        y_2 = model_2.predict(x, verbose=0)
         l = np.argmax(y_1+y_2, axis=1)
         for i in range(len(faces)):
             cv2.rectangle(frame, (pos[i][0],pos[i][1]),
@@ -186,9 +189,29 @@ def FacemeshRecognition():
     ret, frame = cap.read()
     frame, de, left_eye_closed, right_eye_closed = inference_facemesh(frame)
     if de:
-        displacement_eye = ((displacement_eye[0]*0.8)+(de[0]*0.2), (displacement_eye[1]*0.8)+(de[1]*0.2))    
+        displacement_eye = ((displacement_eye[0]*0.8)+(de[0]*0.2), (displacement_eye[1]*0.8)+(de[1]*0.2))
+    displacement_eye = (max(min(1, displacement_eye[0]), -1), max(min(0.3, displacement_eye[1]), -0.3))
+    return frame    
 
 def EmotionRecognition():
     global AutomaticExpression
     ret, frame = cap.read()
     frame, AutomaticExpression = inference_emotionrecog(frame)
+    return frame
+
+if __name__ == '__main__':
+    while True:
+        frameA = FacemeshRecognition()
+        frameB = EmotionRecognition()
+        frame = cv2.hconcat([frameA, frameB])
+        if left_eye_closed and right_eye_closed:
+            print('BOTH EYES CLOSED')
+        elif left_eye_closed:
+            print('LEFT EYE CLOSED')
+        elif right_eye_closed:
+            print('RIGHT EYE CLOSED')
+        else:
+            print(displacement_eye)
+        cv2.imshow('Facemesh', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
