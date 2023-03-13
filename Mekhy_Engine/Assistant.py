814d986bc39f5be5613eafe6dca2fef0aafbc607
Felipe Catapano Emrich Melo <mefkhytheroboticist@gmail.com>
2023-03-13 12:50:46 -0300

    Assistant prompt bug fix

--- Mekhy_Engine/Assistant.py
+++ Mekhy_Engine/Assistant.py
@@ -15,22 +15,19 @@ recorder = PvRecorder(device_index=-1, frame_length=porcupine.frame_length)
 def record_query():
     wavfile = wave.open("resources/query.wav", "wb")
     wavfile.setparams((1, 2, 16000, 512, "NONE", "NONE"))
+    pcms = []
     for i in range(0, int(16000 / 512 * 5)):
         pcm = recorder.read()
+        pcms.append(pcm)
+    for pcm in pcms:
         wavfile.writeframes(struct.pack("h" * len(pcm), *pcm))
     wavfile.close()
 
-def trigger():
-    SoundEffects.PlayOnDemand("resources/assistant_listening.wav", remove_file=False)
-    time.sleep(0.5)
-    record_query()
-    SoundEffects.PlayOnDemand("resources/assistant_ok.wav", remove_file=False)
-    with open("resources/query.wav", "rb") as query:
-        transcript = openai.Audio.transcribe("whisper-1", query)
-    query.close()
+def assistant_query(query):
+    query = query.capitalize()
     completion = openai.Completion.create(
         engine="davinci",
-        prompt=transcript,
+        prompt=f"Question: {query}\nAnswer: ",
         temperature=0.9,
         max_tokens=150,
         top_p=1,
@@ -38,7 +35,23 @@ def trigger():
         presence_penalty=0,
         stop=["\n", " User:", " AI:"],
     )
-    SoundEffects.TTS(completion.choices[0].text)
+    answer = completion.choices[0].text
+    return answer
+
+def trigger():
+    print("Triggered")
+    SoundEffects.PlayOnDemand("resources/assistant_listening.wav", remove_file=False)
+    time.sleep(0.5)
+    print("Recording")
+    record_query()
+    SoundEffects.PlayOnDemand("resources/assistant_ok.wav", remove_file=False)
+    with open("resources/query.wav", "rb") as query:
+        transcript = openai.Audio.transcribe("whisper-1", query)['text']
+    query.close()
+    print(transcript)
+    answer = assistant_query(transcript)
+    print(answer)
+    SoundEffects.TTS(answer)
 
 def start():
     recorder.start()
