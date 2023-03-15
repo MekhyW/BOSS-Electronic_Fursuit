3fab84145ba01c6a3bdef786d15517ece9c1e438
Felipe Catapano Emrich Melo <mefkhytheroboticist@gmail.com>
2023-03-14 23:36:49 -0300

    Testing performance opencv without resize/duplicate

--- Mekhy_Engine/Displays.py
+++ Mekhy_Engine/Displays.py
@@ -103,9 +103,10 @@ def PlayVideo(file_name, remove_file=True):
     while(cap.isOpened() and playingvideo):
         ret, frame = cap.read()
         if ret:
-            resized = cv2.resize(frame, (display_width, display_height), interpolation = cv2.INTER_AREA)
-            duplicated = cv2.hconcat([resized, resized])
-            cv2.imshow('Eyes', duplicated)
+            #resized = cv2.resize(frame, (display_width, display_height), interpolation = cv2.INTER_AREA)
+            #duplicated = cv2.hconcat([resized, resized])
+            #cv2.imshow('Eyes', duplicated)
+            cv2.imshow('Eyes', frame)
             if cv2.waitKey(25) & 0xFF == ord('q'):
                 break
         else:
