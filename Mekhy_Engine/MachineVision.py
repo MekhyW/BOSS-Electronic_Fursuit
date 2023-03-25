cd9a9518e7c509c0fcba1fa125ec8ddd3008ea6e
Felipe Catapano Emrich Melo <mefkhytheroboticist@gmail.com>
2023-03-24 21:00:13 -0300

    Optimizing inference_facemesh

--- Mekhy_Engine/MachineVision.py
+++ Mekhy_Engine/MachineVision.py
@@ -5,11 +5,9 @@ displacement_eye = (0,0)
 left_eye_closed = False
 right_eye_closed = False
 AutomaticExpression = 'Neutral'
-mp_face_detection = mp.solutions.face_detection
 mp_face_mesh = mp.solutions.face_mesh
 mp_drawing = mp.solutions.drawing_utils
 drawSpec = mp_drawing.DrawingSpec(thickness=1, circle_radius=2)
-face_detection = mp_face_detection.FaceDetection(min_detection_confidence=0.5)
 face_mesh = mp_face_mesh.FaceMesh(max_num_faces=1, refine_landmarks=True, min_detection_confidence=0.5, min_tracking_confidence=0.5)
 try:
     from picamera.array import PiRGBArray
@@ -35,45 +33,19 @@ RIGHT_IRIS = [473, 474, 475, 476, 477]
 LEFT_EYEBROW = [383, 300, 293, 334, 296, 336, 285, 417]
 RIGHT_EYEBROW = [156, 70, 63, 105, 66, 107, 55, 193]
 
-def inference_facemesh(image):
+def inference_facemesh(image, drawing):
     frame = image.copy()
     H, W, _ = frame.shape
     rgb_image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    results = face_detection.process(rgb_image)
     results_mesh = face_mesh.process(rgb_image)
-    if results.detections:
-        faces = []
-        pos = []
-        for detection in results.detections:
-            box = detection.location_data.relative_bounding_box
-            x = int(box.xmin * W)
-            y = int(box.ymin * H)
-            w = int(box.width * W)
-            h = int(box.height * H)
-            x1 = max(0, x)
-            y1 = max(0, y)
-            x2 = min(x + w, W)
-            y2 = min(y + h, H)
-            face = image[y1:y2,x1:x2]
-            face = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
-            faces.append(face)
-            pos.append((x1, y1, x2, y2))
     if results_mesh.multi_face_landmarks:
-        for faceLms in results_mesh.multi_face_landmarks:
-            mp_drawing.draw_landmarks(frame, faceLms, mp_face_mesh.FACEMESH_CONTOURS,drawSpec,drawSpec)
         mesh_points=np.array([np.multiply([p.x, p.y], [W, H]).astype(int) for p in results_mesh.multi_face_landmarks[0].landmark])
         lex1, ley1 = np.min(mesh_points[LEFT_EYE], axis=0)
         lex2, ley2 = np.max(mesh_points[LEFT_EYE], axis=0)
-        cv2.rectangle(frame, (lex1, ley1), (lex2, ley2), (0, 255, 0), 2)
         rex1, rey1 = np.min(mesh_points[RIGHT_EYE], axis=0)
         rex2, rey2 = np.max(mesh_points[RIGHT_EYE], axis=0)
-        cv2.rectangle(frame, (rex1, rey1), (rex2, rey2), (0, 255, 0), 2)
         (l_cx, l_cy), l_radius = cv2.minEnclosingCircle(mesh_points[LEFT_IRIS])
         (r_cx, r_cy), r_radius = cv2.minEnclosingCircle(mesh_points[RIGHT_IRIS])
-        center_left = np.array([l_cx, l_cy], dtype=np.int32)
-        center_right = np.array([r_cx, r_cy], dtype=np.int32)
-        cv2.circle(frame, center_left, int(l_radius), (255,0,255), 1, cv2.LINE_AA)
-        cv2.circle(frame, center_right, int(r_radius), (255,0,255), 1, cv2.LINE_AA)
         displacement_left_eye = (2*(l_cx-((lex1+lex2)/2))/abs(lex2-lex1), 2*(l_cy-((ley1+ley2)/2))/abs((ley2-ley1)))
         displacement_right_eye = (2*(r_cx-((rex1+rex2)/2))/abs(rex2-rex1), 2*(r_cy-((rey1+rey2)/2))/abs((rey2-rey1)))
         displacement_eye = ((displacement_left_eye[0]+displacement_right_eye[0])/2, (displacement_left_eye[1]+displacement_right_eye[1])/2)
@@ -85,6 +57,15 @@ def inference_facemesh(image):
             right_eye_closed = True
         else:
             right_eye_closed = False
+        if drawing:
+            for faceLms in results_mesh.multi_face_landmarks:
+                mp_drawing.draw_landmarks(frame, faceLms, mp_face_mesh.FACEMESH_CONTOURS,drawSpec,drawSpec)
+            cv2.rectangle(frame, (lex1, ley1), (lex2, ley2), (0, 255, 0), 2)
+            cv2.rectangle(frame, (rex1, rey1), (rex2, rey2), (0, 255, 0), 2)
+            center_left = np.array([l_cx, l_cy], dtype=np.int32)
+            center_right = np.array([r_cx, r_cy], dtype=np.int32)
+            cv2.circle(frame, center_left, int(l_radius), (255,0,255), 1, cv2.LINE_AA)
+            cv2.circle(frame, center_right, int(r_radius), (255,0,255), 1, cv2.LINE_AA)
         return frame, displacement_eye, left_eye_closed, right_eye_closed
     return frame, None, True, True
 
@@ -97,10 +78,10 @@ def getFrame():
         ret, frame = cap.read()
     return frame
 
-def FacemeshRecognition():
+def FacemeshRecognition(drawing=False):
     global displacement_eye, left_eye_closed, right_eye_closed
     frame = getFrame()
-    frame, de, left_eye_closed, right_eye_closed = inference_facemesh(frame)
+    frame, de, left_eye_closed, right_eye_closed = inference_facemesh(frame, drawing)
     if de:
         displacement_eye = ((displacement_eye[0]*0.8)+(de[0]*0.2), (displacement_eye[1]*0.8)+(de[1]*0.2))
     displacement_eye = (max(min(1, displacement_eye[0]), -1), max(min(0.3, displacement_eye[1]), -0.3))
@@ -108,7 +89,7 @@ def FacemeshRecognition():
 
 if __name__ == '__main__':
     while True:
-        frame = FacemeshRecognition()
+        frame = FacemeshRecognition(drawing=True)
         if left_eye_closed and right_eye_closed:
             print('BOTH EYES CLOSED')
         elif left_eye_closed:
