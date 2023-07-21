8c1c75bdfcd6aa470e035911dd62304ee24f5505
Felipe Catapano Emrich Melo <mefkhytheroboticist@gmail.com>
2023-07-21 14:21:20 -0300

    Update DependencyInstaller.sh

--- DependencyInstaller.sh
+++ DependencyInstaller.sh
@@ -9,18 +9,24 @@
 #
 # -----------------------------------------------------------------------------
 sudo apt-get update
-sudo apt-get install -y git-all
+sudo apt-get install -y cmake
 sudo apt-get install -y python3-pip
 sudo apt install -y ffmpeg
 sudo apt install -y sox
 sudo apt install -y python3-gi gir1.2-wnck-3.0
 sudo apt install -y libsdl2-mixer-2.0-0
 sudo apt install -y arduino
+sudo apt-get install -y libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev
+sudo apt-get install -y libavcodec-dev libavformat-dev libswscale-dev libv4l-dev 
+sudo apt-get install -y libxvidcore-dev libx264-dev
+sudo apt-get install -y libgtk2.0-dev
+sudo apt-get install -y libatlas-base-dev gfortran
 # PIP PACKAGES
 sudo pip3 install --upgrade pip
 sudo pip3 install telepota
 sudo pip3 install googletrans==3.1.0a0
-sudo pip3 install opencv-python
+sudo pip3 install numpy
+sudo apt install -y python3-opencv 
 sudo pip3 install picamera[array]
 sudo pip3 install pygame --upgrade
 sudo pip3 install spotdl
@@ -40,9 +46,9 @@ sudo nano /etc/xdg/lxsession/LXDE-pi/autostart
 cd ..
 # SETUP
 # - set auto login and password for pi user
-# - setup VNC connection for 4g hotspot (remember to use 5GHz!!, 2.4GHz is too slow to work)
+# - setup VNC connection for 4g hotspot (remember to try both 2.4GHz and 5GHz when tethering)
 # - setup dual display scheme
 # - disable screen blanking
 # - insert credentials into Mekhy_Engine/resources/credentials.json
-# - set app bar width to zero
+# - set app bar to hide when unused (with hidden size of 0)
 # - enable CSI camera
