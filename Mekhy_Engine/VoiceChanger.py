60b06b98f8760a87912f88beeb25ad59fcec275c
Felipe Catapano Emrich Melo <mefkhytheroboticist@gmail.com>
2023-10-05 16:42:52 -0300

    Revert "Update VoiceChanger.py"
    This reverts commit 04e95cc80cea0de728b972454b3b6c3386d2df1b.

--- Mekhy_Engine/VoiceChanger.py
+++ Mekhy_Engine/VoiceChanger.py
@@ -1,29 +1,27 @@
 import os
 import time
 import threading
-os.system("pactl unload-module module-echo-cancel")
-os.system("pactl load-module module-echo-cancel aec_method=webrtc source_name=echocancel sink_name=echocancel1")
-os.system("pacmd set-default-source echocancel")
-os.system("pacmd set-default-sink echocancel1")
+#os.system("pactl load-module module-echo-cancel")
+#os.system("pacmd set-default-source alsa_input.usb-Razer_Razer_Kraken_V3_X_00000000-00.iec958-stereo.echo-cancel")
 
 def sox_thread(voice):
     print("Killing SoX windows")
     os.system("killall play")
     print("Setting new voice")
     if voice == 'Mekhy':
-        os.system("lxterminal -e play '|rec -c 2 -b 16 -r 44100 --buffer 128 -d pitch -300 band 40 5k'")
+        os.system("lxterminal -e play '|rec -c 2 -b 16 -r 44100 --buffer 128 -d pitch -300 vol 10 band 40 5k'")
     elif voice == 'Demon':
-        os.system("lxterminal -e play '|rec -c 2 -b 16 -r 44100 --buffer 128 -d pitch -600 bass 30 reverb band 40 5k'")
+        os.system("lxterminal -e play '|rec -c 2 -b 16 -r 44100 --buffer 128 -d pitch -600 bass 30 reverb vol 10 band 40 5k'")
     elif voice == 'Voice of Conscience':
-        os.system("lxterminal -e play '|rec -c 2 -b 16 -r 44100 --buffer 128 -d echo 0.8 0.9 200 0.3 bass 30 reverb band 40 5k'")
+        os.system("lxterminal -e play '|rec -c 2 -b 16 -r 44100 --buffer 128 -d echo 0.8 0.9 200 0.3 bass 30 reverb vol 10 band 40 5k'")
     elif voice == 'Baby':
-        os.system("lxterminal -e play '|rec -c 2 -b 16 -r 44100 --buffer 128 -d pitch 400 bass 20 band 40 5k'")
+        os.system("lxterminal -e play '|rec -c 2 -b 16 -r 44100 --buffer 128 -d pitch 400 bass 20 vol 10 band 40 5k'")
     elif voice == 'Chipmunk':
-        os.system("lxterminal -e play '|rec -c 2 -b 16 -r 44100 --buffer 128 -d pitch 800 band 40 5k'")
+        os.system("lxterminal -e play '|rec -c 2 -b 16 -r 44100 --buffer 128 -d pitch 800 vol 10 band 40 5k'")
     elif voice == 'Radio':
-        os.system("lxterminal -e play '|rec -c 2 -b 16 -r 44100 --buffer 128 -d downsample 10 band 40 5k'")
+        os.system("lxterminal -e play '|rec -c 2 -b 16 -r 44100 --buffer 128 -d downsample 10 vol 10 band 40 5k'")
     elif voice == 'No Effects':
-        os.system("lxterminal -e play '|rec -c 2 -b 16 -r 44100 --buffer 512 -d band 40 5k'")
+        os.system("lxterminal -e play '|rec -c 2 -b 16 -r 44100 --buffer 512 -d vol 10 band 40 5k'")
     elif voice == 'Mute':
         os.system("lxterminal -e play '|rec -c 2 -b 16 -r 44100 --buffer 512 -d vol 0'")
 
